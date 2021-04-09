4ead9c3daa7518dc497e16d0ebcbbc9a049a9e94
alvin mallari <ey.em716@gmail.com>
2021-04-09 14:47:50 +0800

    if else expressions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 function App() {
   const name = 'Alvin'
+  const x = false
   return (
     <div className="App">
       <h1>Hello From React</h1>
-      <h2>Hello {name}</h2>
+      <h2>Hello {x ? 'Yes' : 'No'}</h2>
     </div>
   );
 }
