e5363a12948e67050fc46bda828d6e5cb84922fa
alvin mallari <ey.em716@gmail.com>
2021-04-09 14:59:17 +0800

    create and import header.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import Header from './components/Header'
+
 function App() {
-  const name = 'Alvin'
-  const x = false
+  
   return (
-    <div className="App">
-      <h1>Hello From React</h1>
-      <h2>Hello {x ? 'Yes' : 'No'}</h2>
+    <div className="container">
+     <Header />
     </div>
   );
 }
