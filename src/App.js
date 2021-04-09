87a050a74c4d8e1ab0b1a619c4ffd5b22daaa5ec
alvin mallari <ey.em716@gmail.com>
2021-04-09 15:14:25 +0800

    add props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Header from './components/Header'
 
 
-// function App() {
+const App = () => {
   
-//   return (
-//     <div className="container">
-//      <Header />
-//     </div>
-//   );
-// }
+  return (
+    <div className="container">
+     <Header title='Hello' />
+    </div>
+  );
+}
 
 
 export default App;
